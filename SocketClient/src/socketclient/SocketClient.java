8c8bfdcbdfef6b4d0e4b9935711b560151b8a81e
Alexander <aborbon@itcr.ac.cr>
2019-10-06 22:22:52 -0600

    Estoy agregando un cliente de pruebas para el servidor de sockets

---
+++ SocketClient/src/socketclient/SocketClient.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package socketclient;
+
+/**
+ *
+ * @author alexander
+ */
+public class SocketClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
