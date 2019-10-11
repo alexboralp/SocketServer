69bff7e338b3329e89ee975a62ac54b1379f157c
aborbon <aborbon@MA-77309.mshome.net>
2019-10-11 01:44:04 -0600

    Arreglo en dos mensajes del server

--- Auctions/src/auctions/MessageServerFactory.java
+++ Auctions/src/auctions/MessageServerFactory.java
@@ -12,12 +12,10 @@ import observerserver.ObserverServerMessageFactory;
  * @author aborbon
  */
 public class MessageServerFactory extends ObserverServerMessageFactory {
-    public static final int NEW_OFFER = 201;
     public static final int MESSAGE_TO_BIDDER = TEXT_MESSAGE_TO_OBSERVER;
     public static final int SENDING_ALL_AUCTIONS = OBSERVABLES_LIST;
     public static final int SENDING_ALL_BIDDERS = OBSERVERS_LIST;
     public static final int SENDING_AUCTION = SENDING_OBSERVABLE;
     public static final int SENDING_BIDDER = SENDING_OBSERVER;
     public static final int MESSAGE_TO_AUCTIONEER = TEXT_MESSAGE_TO_OBSERVER;
-    public static final int OFFER_ACCEPTED = 202;
 }
